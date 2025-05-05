d9dba39bd39b6e4ab97bd158ff52124af48ef108
Bahka <denis_raus@yahoo.com>
2025-05-05 11:26:58 +0300

    first commit

--- app/config/mongodb.config.js
+++ app/config/mongodb.config.js
@@ -1,6 +1,6 @@
 module.exports = {
    // url: 'mongodb://localhost:27017/prw08'
-   url: 'mongodb://student:h3T-bYhx-uW8@localhost:27017/sandbox'
+   url: 'mongodb+srv://dbUser1:creeper64200@cluster0.lqsas2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
 
 
